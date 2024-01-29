63c1854e0ee1457ace88e68461266d092ddf5b40
Ravindu612 <dravindu120@gmail.com>
2024-01-29 03:24:37 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+
+<!--this is printing hello world-->
+
+<?php
+
+//this is printing hello world
+/*multiple line
+comment
+
+*/
+echo "<h1>Hello world</h1>";
+?>
+    
+</body>
+</html>
+
+
